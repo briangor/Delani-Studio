c681f92fe3b7fb8d3918aa0913934f466b891954
Brian <briangor13@gmail.com>
2020-09-15 04:02:09 -0600

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,29 @@
+/*
+$(document).ready(function() {
+
+    $('.photo').hover(function() {
+        $(this)
+            .find('.caption')
+            .stop()
+            .animate({ bottom: '0' }, { duration: 1600, easing: 'easeOutQuart' });
+
+
+    }, function() {
+
+        $(this)
+            .find('.caption')
+            .stop()
+            .animate({ bottom: '-100px' }, { duration: 1600, easing: 'easeOutQuart' });
+
+    });
+});
+/*
+<script>
+        $(document).ready(function() {
+            $("imgPortfolio").hover(function() {
+                $(this).css("background-color", "green");
+            }, function() {
+                $(this).css("background-color", "yellow");
+            });
+        });
+    </script>*/
